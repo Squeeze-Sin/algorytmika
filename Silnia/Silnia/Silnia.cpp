52363a51d1f92836a3814dfcabfd7de4632f9649
Squeeze-Sin <74215621+Squeeze-Sin@users.noreply.github.com>
2023-11-08 10:35:28 +0100

    Add files via upload

---
+++ Silnia/Silnia/Silnia.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+long long silnia(int n)
+{
+    if (n < 2)
+        return 1; 
+    return n * silnia(n - 1); 
+}
+
+int main()
+{
+    int n;
+    cout << "Podaj liczbe: ";
+    cin >> n;
+    cout << "Silnia = " << silnia(n);
+
+
+    return 0;
+}
